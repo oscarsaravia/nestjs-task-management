bc557f76c2695740125840b35dc393ec5bdff6ed
Oscar Saravia <oscarsaraviard0013@gmail.com>
2025-05-21 13:55:35 -0600

    feat: tasks CRUD created
    created the neccesary controllers and services to handle CRUD requests to the API

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = []
+
+  getAllTasks(): Task[] {
+    return this.tasks
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto
+    let tasks = this.getAllTasks()
+    if (status) {
+      tasks = tasks.filter((task) => {
+        return task.status === status
+      })
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return  true
+        }
+        return false
+      })
+    }
+
+    return tasks
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((item) => {
+      return item.id === id
+    })
+    if (task) return task
+    return { id: '', title: '', description: '', status: TaskStatus.DONE}
+  }
+
+  createTask(createTAskDto: CreateTaskDto): Task {
+    const { title, description } = createTAskDto
+    const task: Task = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: TaskStatus.OPEN
+    }
+    this.tasks.push(task)
+    return task
+  }
+
+  deleteTask(id: string): Task[] {
+    const newTasks = this.tasks.filter((task) => {
+      return task.id !== id
+    })
+    this.tasks = newTasks
+    return this.tasks
+  }
+
+  updateTaskStatus(id:string, status: TaskStatus): Task {
+    const task = this.getTaskById(id)
+    task.status = status
+    return task
+  }
+}
